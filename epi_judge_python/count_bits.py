21dbe03cf27056a16bb4e9559fb3f901ad9b2e7b
Ki Song <kiwisquash@gmail.com>
2019-07-07 18:38:35 -0700

    Added some solutions to count_bits.py.

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -3,7 +3,33 @@ from test_framework import generic_test
 
 def count_bits(x):
     # TODO - you fill in here.
-    return 0
+
+    # Brute force O(n), n = number of bits
+    # count = 0
+    # while x:
+    #     count += x & 1
+    #     x >>= 1
+    # return count
+
+    # Count the number of 1s by clearing out the lowest "1" each time. 
+    # O(k), k = number of 1s
+    # count = 0
+    # while x:
+    #     count += 1
+    #     x &= (x-1)
+    # return count
+
+    # Assuming that all numbers are 64 bits, we can use caching.
+    # Strategy: 
+    #    - Precompute the counts for all 16 bit numbers. 
+    #    - Algorithm divides the 64 bit number into 4 pieces
+    #    - Calculate the number of 1s in each piece
+    #    - Add
+    
+    cache_cap = 2**16
+    cache = []
+    for x in range(cache_cap):
+        cache.append(count_small_bits(x))
 
 
 if __name__ == '__main__':
