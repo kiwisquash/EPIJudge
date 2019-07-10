c5853f8c608cd6ca602d30ce010e40cdfde17e93
Ki Song <kiwisquash@gmail.com>
2019-07-09 21:49:25 -0700

    ch5 reading: Added my solution and book solution to even_odd.py

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -8,8 +8,19 @@ from test_framework.test_utils import enable_executor_hook
 
 def even_odd(A):
     # TODO - you fill in here.
-    return
-
+    # i = 0
+    # for j in range(len(A)):
+    #     if A[j] % 2 == 0:
+    #         A[i], A[j] = A[j], A[i]
+    #         i+=1
+    next_even = 0
+    next_odd = len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -=1
 
 @enable_executor_hook
 def even_odd_wrapper(executor, A):
