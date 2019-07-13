7a0320923f7f74638e76d67d6295c2cbd5644264
Ki Song <kiwisquash@gmail.com>
2019-07-12 23:01:02 -0700

    chap 9 prob1: Added a space-inefficient solution. Study this problem carefully.

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,20 @@
 from test_framework import generic_test
 
-
-def is_balanced_binary_tree(tree):
+def is_balanced_binary_tree(tree, heights = None):
     # TODO - you fill in here.
-    return True
+    if tree is None: return True
+    
+    if heights is None:
+        heights = {}
 
+    def tree_height(tree):
+        if tree is None: return -1
+        if tree not in heights:
+            heights[tree] = max(tree_height(tree.left), tree_height(tree.right)) + 1
+        return heights[tree]
+
+    height_diff = abs(tree_height(tree.left) - tree_height(tree.right))
+    return height_diff <= 1 and is_balanced_binary_tree(tree.left, heights) and is_balanced_binary_tree(tree.right, heights)
 
 if __name__ == '__main__':
     exit(
