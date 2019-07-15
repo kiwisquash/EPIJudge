1bb0deda090f9b08174a808d78b2f35edf90ab1d
Ki Song <kiwisquash@gmail.com>
2019-07-15 00:42:20 -0700

    chap 11 problem 1: Added a good solution to search_first_key. Revisit.

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    L = 0
+    R = len(A) - 1
+    # if R == -1: return -1 # this is only needed if I plan to reference M outside of the while loop
+    while L <= R:
+        M = L + (R - L) // 2
+        if A[M] < k:
+            L = M + 1
+        elif A[M] > k:
+            R = M - 1
+        else: # A[M] is k here
+            if M == 0 or A[M-1]!=k: return M 
+            R = M - 1
+    return -1
+    # This gives O(n) worst case. Is that what we want to settle for?
+            # break
+    # while M>=0 and A[M] == k:
+        # M-=1
+    # return M + 1 if L <= R else -1 
 
 
 if __name__ == '__main__':
