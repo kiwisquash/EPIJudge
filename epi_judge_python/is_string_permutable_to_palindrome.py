432c48965ca0c8512ab30456f56c4ca88be80194
Ki Song <kiwisquash@gmail.com>
2019-07-15 06:54:35 -0700

    ch12 prob1: Final improvement: Near-one-liner solution.

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,16 +1,17 @@
 from test_framework import generic_test
 
 from collections import Counter
+from functools import reduce
 
 def can_form_palindrome(s):
     # A palindrome of even length has even number of letters per char
     # A palindrome of odd length has exactly one char that is odd number, all others even
     char_nums = Counter(s)
-    num_odds = 0
-    for char in char_nums:
-        num_odds += char_nums[char] % 2 # Counts the number of odds
-    return num_odds <= 1
-
+    # num_odds = 0
+    # for char in char_nums:
+    #     num_odds += char_nums[char] % 2 # Counts the number of odds
+    # return num_odds <= 1
+    return reduce(lambda x,y: x+y, [x % 2 for x in char_nums.values()],0) <=1 #I could also use 'sum'
 
 if __name__ == '__main__':
     exit(
