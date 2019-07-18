5d492fbe5a004f3184f4d97579049b946ec791c5
Ki Song <kiwisquash@gmail.com>
2019-07-18 14:50:47 -0700

    Added my recursive solution.

--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def gcd(x, y):
-    # TODO - you fill in here.
-    return 0
+    if x==0 or y==0:
+        return x if y == 0 else y
+    if x > y:
+        return gcd(x % y, y)
+    return gcd(x, y % x)
 
 
 if __name__ == '__main__':
