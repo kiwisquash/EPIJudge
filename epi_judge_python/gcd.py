4d5c89ac3fabc39a555cdf034deecf589a473bf0
Ki Song <kiwisquash@gmail.com>
2019-07-18 14:56:16 -0700

    Added iterative sol using a while loop that uses O(1) space.

--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -2,12 +2,9 @@ from test_framework import generic_test
 
 
 def gcd(x, y):
-    if x==0 or y==0:
-        return x if y == 0 else y
-    if x > y:
-        return gcd(x % y, y)
-    return gcd(x, y % x)
-
+    while y != 0:
+        x, y = y, x % y
+    return x
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main("gcd.py", 'gcd.tsv', gcd))
