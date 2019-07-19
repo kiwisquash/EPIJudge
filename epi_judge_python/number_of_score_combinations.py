2178e76818a66a89bc456cd63351f007e1647fcf
Ki Song <kiwisquash@gmail.com>
2019-07-19 12:40:34 -0700

    Falty solution... not sure if I really understand the full solution.

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test
+from copy import copy
 
 
-def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+sub_memo = {
+    1: 0,
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 2,
+    7: 1,
+}
+
+memo = sub_memo.copy()
+
+memo[7] +=1
+
+def num_combinations_for_final_score(n, individual_play_scores):
+    if n not in memo:
+        if n - 3 not in sub_memo:
+            sub_memo[n-3] = sub_memo[n-6] + 1^((n-5)%2)
+        memo[n] = memo[n-7] + sub_memo[n-3] + 1^((n-2)%2)
+    return memo[n] 
 
 
 if __name__ == '__main__':
