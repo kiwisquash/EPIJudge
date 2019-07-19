4178d04f02363a7c7804e26fb9c70ee8ba02e145
Ki Song <kiwisquash@gmail.com>
2019-07-18 21:59:35 -0700

    Added O(1) memory solution.

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -1,10 +1,13 @@
 from test_framework import generic_test
 
 
-def fibonacci(n):
-    # TODO - you fill in here.
-    return -1
 
+def fibonacci(n):
+    prev = 1
+    next = 0
+    for _ in range(n):
+        prev, next = next, prev + next
+    return next
 
 if __name__ == '__main__':
     exit(
