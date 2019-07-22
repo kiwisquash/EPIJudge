9470c0c22b346d86568217f3cb2cd474219ea78d
Ki Song <kiwisquash@gmail.com>
2019-07-21 17:47:07 -0700

    Adding locks didn't quite work...

--- epi_judge_python/sync_two_interweaving_threads.py
+++ epi_judge_python/sync_two_interweaving_threads.py
@@ -4,21 +4,28 @@ import time
 CAP = 100
 
 output_list = []
-dt = 1
+dt = .1
 
 def print_odds():
     for i in range(1,CAP,2):
-        print(i)
-        output_list.append(i)
-        time.sleep(dt)
+        with lock:
+            while (output_list and output_list[-1] % 2 == 1): 
+                time.sleep(dt)
+            print(i)
+            output_list.append(i)
 
 def print_evens():
+    print('KS')
+    print(output_list)
     for i in range(2,CAP+1,2):
-        print(i)
-        output_list.append(i)
-        time.sleep(dt)
+        with lock:
+            while (output_list and output_list[-1] % 2 == 0): 
+                time.sleep(dt)
+            output_list.append(i)
+            time.sleep(dt)
 
 if __name__ == "__main__":
+    lock = threading.Lock()
     t1 = threading.Thread(target=print_odds)
     t2 = threading.Thread(target=print_evens)
 
